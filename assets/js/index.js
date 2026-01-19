dacd0024b95087df7609f484e7fb994c27a2e67a
BilousOleg <olezhka82802@gmail.com>
2026-01-19 13:44:04 +0200

    Optimize isPalindrome function using array prototype methods

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,17 +4,10 @@
  * @returns {boolean}
  */
 function isPalindrom(inputString) {
-  let isPalindromFlag = false; // Булева змінна-перемикач (флаг) для запису і повернення значення
-  // Цикл перебору рядка одночасно з двох кінців, щоб дізнатись - він паліндром, чи ні
-  for (let i = 0; i < inputString.length / 2; i++) {
-    if (inputString[i] !== inputString[inputString.length - i - 1]) {
-      isPalindromFlag = false; // Оновлення змінної, якщо вона була true і неспівпадіння знайдено
-      break;
-    } else {
-      isPalindromFlag = true; // Оновлення змінної, якщо неспівпадіння немає
-    }
-  }
-  return isPalindromFlag;
+  // Перевірка на те, чи кожен символ рядка дорівнює символу перегорнутого рядка з цим же індексом,
+  return [...inputString].every(
+    (char, index, array) => char === array.toReversed()[index],
+  );
 }
 
 const str4 = 'qwertrewq';
